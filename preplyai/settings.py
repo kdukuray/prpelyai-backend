7a803464bb471bbb627459d12ed343973826bc82
kdukuray <kdukuray01@gmail.com>
2024-12-11 14:04:58 -0500

    set debug value

--- preplyai/settings.py
+++ preplyai/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-(u&yas5^2u=!g3rikkt!)cvj7%b6n_jug8swo%uj^9@uy_mr!z'
 SECRET_KEY = os.environ.get('PREPLYAI_BACKEND_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 ALLOWED_HOSTS = ["*"]
 
 
