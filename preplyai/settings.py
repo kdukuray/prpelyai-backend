f8d389bea13a8646eab9ba9fc3593d762c11a054
kdukuray <kdukuray01@gmail.com>
2024-12-11 16:01:49 -0500

    updated cors

--- preplyai/settings.py
+++ preplyai/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://preplyai-frontend.vercel.app/"
 ]
 django_heroku.settings(locals())
 
