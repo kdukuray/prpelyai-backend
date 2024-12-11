10c715694ff4452a48ffc74bd840d307929df600
kdukuray <kdukuray01@gmail.com>
2024-12-11 12:25:32 -0500

    deleted dead code

--- preplyai/settings.py
+++ preplyai/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -133,5 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Things added by me
 CORS_ALLOW_ALL_ORIGINS = True
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
+
