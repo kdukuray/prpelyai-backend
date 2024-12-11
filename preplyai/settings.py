7773ab62dfe78c09458bd119511d9620d7f21cce
kdukuray <kdukuray01@gmail.com>
2024-12-11 14:49:38 -0500

    attempt to fix cors

--- preplyai/settings.py
+++ preplyai/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,8 +134,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Things added by me
-CORS_ALLOW_ALL_ORIGINS = True
-
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 django_heroku.settings(locals())
 
 
