c410942de72c97946129d254b0690cb2e75982b6
kdukuray <kdukuray01@gmail.com>
2025-01-03 21:06:47 -0500

    changed generate summary view into asynchronous function

--- summary/views.py
+++ summary/views.py
@@ -1,12 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from openai import OpenAI
+from openai import OpenAI, AsyncOpenAI
 from os import environ
 from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from .models import Summary
 from .serializers import SummarySerializer
+import asyncio
+import time
+from adrf.decorators import api_view as async_api_view
+from asgiref.sync import sync_to_async
 
 @api_view(['POST'])
 def get_generations(request):
@@ -20,26 +24,40 @@ def get_generations(request):
         print(str(e))
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
-def generate_summary(request):
+@sync_to_async
+def save_summary(summary: Summary):
+    summary.save()
+@async_api_view(['POST'])
+async def generate_summary(request):
     try:
-        client = OpenAI(api_key=environ.get('OPENAI_API_KEY'))
-        chunk_summary_system_prompt = """You are an advanced language model tasked with extracting all important and valuable data from a 
-        given piece of text. Your goal is not to summarize the text but to identify and extract full sentences that convey the 
-        key points, significant information, or critical details.
-        Instructions:
-        Analyze the text to determine which sentences contain essential information.
-        Select and extract complete sentences that:
-        Present new facts, ideas, or key arguments.
-        Provide significant context or details crucial for understanding the text.
-        Avoid redundancy by excluding repeated information.
-        Retain the original wording and sentence structure from the input text. Do not rephrase, paraphrase, or summarize
-         unless it is needed to avoid redundancy and repetition.
-        Maintain any other important context provided within the section. 
-        Ensure that the extracted sentences are presented in their original order for coherence. Omit filler, background, 
-        or tangential information unless directly relevant to the important points.
-        The response should be single paragraph with all the points concatenated next to each other.
-        """
+        async_client = AsyncOpenAI(api_key=environ.get("OPENAI_API_KEY"))
+        sync_client = OpenAI(api_key=environ.get('OPENAI_API_KEY'))
+
+        async def summarize_chunk(text_chunk: str) -> str:
+            chunk_summary_system_prompt = """You are an advanced language model tasked with extracting all important and valuable data from a 
+                    given piece of text. Your goal is not to summarize the text but to identify and extract full sentences that convey the 
+                    key points, significant information, or critical details.
+                    Instructions:
+                    Analyze the text to determine which sentences contain essential information.
+                    Select and extract complete sentences that:
+                    Present new facts, ideas, or key arguments.
+                    Provide significant context or details crucial for understanding the text.
+                    Avoid redundancy by excluding repeated information.
+                    Retain the original wording and sentence structure from the input text. Do not rephrase, paraphrase, or summarize
+                     unless it is needed to avoid redundancy and repetition.
+                    Maintain any other important context provided within the section. 
+                    Ensure that the extracted sentences are presented in their original order for coherence. Omit filler, background, 
+                    or tangential information unless directly relevant to the important points.
+                    The response should be single paragraph with all the points concatenated next to each other.
+                    """
+            api_response = await async_client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": chunk_summary_system_prompt},
+                    {"role": "user", "content": chunk},
+                ]
+            )
+            return api_response.choices[0].message.content
 
         pdf_document = PdfReader(request.FILES["source_pdf"])
         pdf_document_text = ""
@@ -55,17 +73,13 @@ def generate_summary(request):
         )
         pdf_document_text_chunks = text_splitter.split_text(pdf_document_text)
         # Summarizing text chunks
-        pdf_document_text_chunks_summary = ""
-        for chunk in pdf_document_text_chunks:
-            api_response = client.chat.completions.create(
-                model="gpt-4o-mini",
-                messages=[
-                    {"role": "system", "content": chunk_summary_system_prompt},
-                    {"role": "user", "content": chunk},
+        tasks = []
+        async with asyncio.TaskGroup() as task_group:
+            for chunk in pdf_document_text_chunks:
+                task = summarize_chunk(chunk)
+                tasks.append(task_group.create_task(task))
 
-                ]
-            )
-            pdf_document_text_chunks_summary += api_response.choices[0].message.content
+        pdf_document_text_chunks_summary = "".join([task.result() for task in tasks])
 
         final_summary_system_prompt = """You are a highly skilled summarization assistant with expertise in creating 
         structured summaries in markdown format. Your task is to generate a comprehensive summary of a document based on the 
@@ -87,7 +101,7 @@ def generate_summary(request):
         readability, making the content accessible to readers who may not have the document.
         """
 
-        final_summary_api_response = client.chat.completions.create(
+        final_summary_api_response = sync_client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
                 {"role": "system", "content": final_summary_system_prompt},
@@ -99,13 +113,13 @@ def generate_summary(request):
 
         new_summary_nav_summary = "New Summary"
         new_summary = Summary(content=final_pdf_summary, nav_summary=new_summary_nav_summary)
-        new_summary.save()
+        await save_summary(new_summary)
+
         return Response({"new_summary_id": new_summary.pk}, status=status.HTTP_200_OK)
 
     except Exception as e:
         print(str(e))
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['GET'])
 def get_summary(request, id):
     try:
